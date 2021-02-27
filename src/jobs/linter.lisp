f7f66675fafe9eabaf2234ff4255d831ac31f938
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-02-28 00:05:28 +0300

    Autogenerate ASD files for linter.

--- src/jobs/linter.lisp
+++ src/jobs/linter.lisp
@@ -3,17 +3,35 @@
   (:import-from #:40ants-ci/steps/sh
                 #:sh)
   (:import-from #:40ants-ci/jobs/lisp-job)
+  (:import-from #:40ants-ci/utils
+                #:current-system-name)
   (:export
    #:linter))
 (in-package 40ants-ci/jobs/linter)
 
 
 (defclass linter (40ants-ci/jobs/lisp-job:lisp-job)
-  ())
+  ((asd-files :initarg :asd-files
+              :initform nil
+              :reader asd-files)))
 
 
-(defun linter ()
-  (make-instance 'linter))
+(defun linter (&key asd-files)
+  "Creates a job which will run SBLint for given ASDF systems.
+
+   If no ASD files given, it will use all ASD files from
+   the current ASDF system."
+  (make-instance 'linter
+                 :asd-files asd-files))
+
+
+(defmethod asd-files :around ((job linter))
+  (let ((files (uiop:ensure-list (call-next-method))))
+    (or files
+        (mapcar #'file-namestring
+                (directory
+                 (asdf:system-relative-pathname (current-system-name)
+                                                #P"*.asd"))))))
 
 
 (defmethod 40ants-ci/jobs/job:steps ((job linter))
@@ -22,5 +40,7 @@
    (list
     (sh "Install SBLint"
         "qlot exec ros install cxxxr/sblint")
-    (sh "Run Linter"
-        "qlot exec sblint *.asd"))))
+    (let ((files (asd-files job)))
+      (sh "Run Linter"
+          (format nil "qlot exec sblint ~{~A~^ ~}"
+                  files))))))
