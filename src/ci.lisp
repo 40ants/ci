860e168b1de2f5ed11678da0b5f2c0318a107a84
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-02-28 18:59:47 +0300

    More items in matrix.

--- src/ci.lisp
+++ src/ci.lisp
@@ -27,12 +27,10 @@
           :os ("ubuntu-latest"
                "macos-latest")
           :quicklisp ("quicklisp"
-                      ;; "ultralisp"
-                      )
+                      "ultralisp")
           :lisp ("sbcl-bin"
-                 ;; "ccl-bin"
-                 ;; "allegro"
-                 )
+                 "ccl-bin"
+                 "allegro")
           ;; :exclude-from-matrix (;; Seems allegro is does not support 64bit OSX.
           ;;                       ;; Unable to install it using Roswell:
           ;;                       ;; alisp is not executable. Missing 32bit glibc?
